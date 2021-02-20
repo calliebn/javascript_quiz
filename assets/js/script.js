e48bb0f7ea7d9c5f4dab12955be6beb2d4ab242e
Callie Nipper <callie.nipper@gmail.com>
2021-02-19 16:04:26 -0800

    Added questions to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,41 @@
 const startButton = document.getElementById('start')
+const startScreen = document.getElementById('start-screen')
 const questionsElement = document.getElementById('questions')
 const qElement = document.getElementById('question-title')
 const answerElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
+let countRightAnswers = 0
+const finalScore = document.getElementById('final-score')
 
+//Timer Variables
 var timeInterval
 let timerElement = document.getElementById('time')
 let clock = 60
 
 let endEl = document.getElementById('end-screen')
 
+
+//Starts game
 startButton.addEventListener('click', startGame)
 
 function startGame() {
 startButton.classList.add('hide')
+startScreen.classList.add('hide')
+
+//Starts the clock
 timeInterval = setInterval(function () { 
     timerElement.textContent = clock;
     clock--
     //If time runs out, stop the clock
-    if (clock<=0) {
+    if (clock < 0) {
         clearInterval(timeInterval);
         endEl.classList.remove('hide')
         questionsElement.classList.add('hide')
     }
 } , 1000);
+
+//Shows questions in a random order
 shuffledQuestions = question.sort(() => Math.random() - .5)
 currentQuestionIndex = 0
 questionsElement.classList.remove('hide')
@@ -37,6 +48,7 @@ function setNextQuestion() {
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+//Question and answer logic
 function showQuestion(question) {
     qElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -52,6 +64,7 @@ function showQuestion(question) {
 
 }
 
+//Stops buttons from duplicating
 function resetState() {
     while (answerElement.firstChild) {
         answerElement.removeChild
@@ -61,49 +74,136 @@ function resetState() {
 
 function selectAnswer(e) {
     const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    Array.from(answerElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
     //Moving to the next question
     currentQuestionIndex++
     if (currentQuestionIndex==question.length) {
         endEl.classList.remove('hide')
         questionsElement.classList.add('hide')
         clearInterval(timeInterval)
+        startScreen.classList.remove('hide')
+        finalScore.innerHTML = clock +1
     } else {
         //Moves questions forward
+        answerElement.innerHTML = ""
         showQuestion(question[currentQuestionIndex])
     }
 
 }
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+} 
+
 const question = [
     {
-        question: "What is question 1?",
+        question: "An array's length can be evaluated which property?",
         answers: [
-            { text: 'Correct Answer', correct: true },
-            { text: 'Wrong Answer 1', correct: false },
-            { text: 'Wrong Answer 2', correct: false },
-            { text: 'Wrong Answer 3', correct: false }
+            { text: '.length', correct: true },
+            { text: '.log', correct: false },
+            { text: 'the console', correct: false },
+            { text: '.loop', correct: false }
 
         ]
     },
 
     {
-        question: "What is question 2?",
+        question: "Within a loop, what can the 'break' keyword be used for?",
         answers: [
-            { text: 'Correct Answer', correct: true },
-            { text: 'Wrong Answer 1', correct: false },
-            { text: 'Wrong Answer 2', correct: false },
-            { text: 'Wrong Answer 3', correct: false }
+            { text: 'break your competitors code', correct: false },
+            { text: 'exit the loop immediately', correct: true },
+            { text: 'repeat the loop', correct: false },
+            { text: 'indicate a stopping condition', correct: false }
 
         ]
     },
 
     {
-        question: "What is question 3?",
+        question: "Which symbol is a string operator?",
         answers: [
-            { text: 'Correct Answer', correct: true },
-            { text: 'Wrong Answer 1', correct: false },
-            { text: 'Wrong Answer 2', correct: false },
-            { text: 'Wrong Answer 3', correct: false }
+            { text: '+', correct: true },
+            { text: '=', correct: false },
+            { text: ':', correct: false },
+            { text: '?', correct: false }
+
+        ]
+    },
+
+    {
+        question: "What is a statement?",
+        answers: [
+            { text: 'Something the bank sends every month', correct: false },
+            { text: 'a series of instructions that a computer can follow one-by one', correct: false },
+            { text: 'An individual instruction or step that is part of a script', correct: true },
+            { text: 'Something a celebrity releases', correct: false }
+
+        ]
+    },
+
+    {
+        question: "How do you start a single line comment in JavaScript?",
+        answers: [
+            { text: '//', correct: true },
+            { text: '/*', correct: false },
+            { text: '<!--', correct: false },
+            { text: '*/', correct: false }
+
+        ]
+    },
+
+    {
+        question: "Which one is not a primitive variable?",
+        answers: [
+            { text: 'string', correct: false },
+            { text: 'number', correct: false },
+            { text: 'object', correct: true },
+            { text: 'boolean', correct: false }
+
+        ]
+    },
+
+    {
+        question: "In an if/else statement, where does the strictest statement go?",
+        answers: [
+            { text: 'at the top', correct: true },
+            { text: 'at the end', correct: false },
+            { text: 'in the middle', correct: false },
+            { text: 'not in an if/else statement', correct: false }
+
+        ]
+    },
+
+    {
+        question: "What does pop do?",
+        answers: [
+            { text: 'adds an element to an array', correct: false },
+            { text: 'removes the last element from an array', correct: true },
+            { text: 'removes the first item from an array', correct: false },
+            { text: 'adds an element to the front of an array', correct: false }
+
+        ]
+    },
+
+    {
+        question: "What is it called when you turn an array into a string?",
+        answers: [
+            { text: 'rope', correct: false },
+            { text: 'liquify', correct: false },
+            { text: 'stringer', correct: false },
+            { text: 'stringify', correct: true }
 
         ]
     }
